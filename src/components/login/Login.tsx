7cb4b03e3a31af1d06fd4f45664e77ff81519819
Premanshu Mukherji <premanshu.mukherji@maersk.com>
2021-10-01 11:16:40 +0530

    Created the project and added a tsx component

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,27 @@
+import React, { FunctionComponent, useState } from "react";
+
+const Login : FunctionComponent<{}> = (props) => {
+
+  const [enteredValue, setEnteredValue] = useState<string>('UserName');
+
+  const onFocusHandler = (event : React.FocusEvent) => {
+    console.log("writing...");
+  };
+
+  const onBlurHandler = (event: React.FocusEvent<HTMLInputElement>) =>{
+    console.log(event);
+    setEnteredValue(event.currentTarget.value)
+  };
+
+  const onChangeHandler = (event: React.FocusEvent<HTMLInputElement>) =>{
+    console.log(event);
+    setEnteredValue(event.currentTarget.value);
+  };
+
+  return (<div> 
+    <input id="userName" type="text" value={enteredValue} onFocus={onFocusHandler} onChange={onChangeHandler} />
+    <input id="userName1" type="text" placeholder="User Email" onBlur={onBlurHandler}  />
+  </div>);
+}
+
+export default Login;
